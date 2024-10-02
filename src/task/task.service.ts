793a8be42b038d8ed667c8589e531a5066848af6
Ewerton Barbosa <ewertonb@live.com>
2024-10-02 18:08:54 -0300

    feat:(tasks) implement crud tasks

---
+++ src/task/task.service.ts
@@ -0,0 +1,35 @@
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { TaskDto } from './task.dto';
+
+@Injectable()
+export class TaskService {
+
+  private tasks: TaskDto[] = [];
+
+  create(task: TaskDto) {
+    this.tasks.push(task);
+    console.log(this.tasks);
+  }
+
+  findById(id: string): TaskDto {
+    const foundTask = this.tasks.filter(t => t.id === id);
+
+    if (foundTask.length > 0) {
+      return foundTask[0]
+    }
+
+    throw new HttpException(`Task id #${id} not found`, HttpStatus.NOT_FOUND);
+  }
+
+  update(task: TaskDto){
+    let taskIndex = this.tasks.findIndex(t => t.id === task.id);
+
+    if(taskIndex >= 0){
+      this.tasks[taskIndex] = task
+      return;
+    }
+
+    throw new HttpException(`Task ${task.id} not found`, HttpStatus.BAD_REQUEST);
+
+  }
+}
